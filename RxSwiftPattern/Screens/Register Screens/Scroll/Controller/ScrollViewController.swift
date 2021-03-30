ae2de9d1e85dee262acd78f3c086763dc64a7f99
Milton Palaguachi <miltonpalaguachi@gmail.com>
2021-03-30 19:40:57 -0400

    creat scroll view controller

---
+++ RxSwiftPattern/Screens/Register Screens/Scroll/Controller/ScrollViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewController.swift
+//  RxSwiftPattern
+//
+//  Created by Milton Palaguachi on 3/30/21.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Hello Scroll View controller"
+        // Do any additional setup after loading the view.
+    }
+}
